3267c48093c2a0e1e44744814e389b75461c4895
rbgksqkr <rbgks1937@naver.com>
2023-12-31 16:13:16 +0900

    feat: rss-parser를 이용하여 velog 게시글 데이터 가져오기(#1)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,27 @@
+'use client';
+
 import styles from './page.module.css';
+import RssParser from 'rss-parser';
 
 const HomePage = () => {
-	return <div>홈페이지</div>;
+	const handleClick = async () => {
+		const CORS_PROXY = 'https://cors-anywhere.herokuapp.com/';
+		const parser = new RssParser();
+		await parser.parseURL(CORS_PROXY + 'https://v2.velog.io/rss/@ghenmaru', function (err, feed) {
+			if (err) throw err;
+			// console.log('feed :', feed);
+			feed.items.forEach(function (entry) {
+				// console.log('entry :', entry);
+				// console.log(entry.title + ':' + entry.link);
+			});
+		});
+	};
+
+	return (
+		<div>
+			<div onClick={handleClick}>호출</div>
+		</div>
+	);
 };
 
 export default HomePage;
