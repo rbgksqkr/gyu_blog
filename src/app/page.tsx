9d229bed67f31a6ed06ebdf64359b3c587cc712b
rbgksqkr <rbgks1937@naver.com>
2024-01-02 00:22:19 +0900

    chore: 사용하지 않는 global css 제거 및 page 디렉토리 코드 정리(#1)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,10 @@
-'use client';
-
-import styles from './page.module.css';
-import RssParser from 'rss-parser';
+import PostSection from '@/components/post/PostSection';
 
 const HomePage = () => {
-	const handleClick = async () => {
-		const CORS_PROXY = 'https://cors-anywhere.herokuapp.com/';
-		const parser = new RssParser();
-		await parser.parseURL(CORS_PROXY + 'https://v2.velog.io/rss/@ghenmaru', function (err, feed) {
-			if (err) throw err;
-			// console.log('feed :', feed);
-			feed.items.forEach(function (entry) {
-				// console.log('entry :', entry);
-				// console.log(entry.title + ':' + entry.link);
-			});
-		});
-	};
-
 	return (
-		<div>
-			<div onClick={handleClick}>호출</div>
-		</div>
+		<>
+			<PostSection />
+		</>
 	);
 };
 
