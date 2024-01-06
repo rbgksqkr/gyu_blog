f1b01735c39e3de713a84635d13330bccae25b76
rbgksqkr <rbgks1937@naver.com>
2024-01-06 15:33:59 +0900

    refactor: axios instance를 생성해 settings파일에서 모든 API error 처리(#5)

--- src/apis/post.ts
+++ src/apis/post.ts
@@ -1,20 +1,13 @@
 import axios from 'axios';
 import { IPost, IUserInfo } from '@/types/post';
+import { Server } from './settings';
 
-export const getVelogPost = async (): Promise<IPost[] | undefined> => {
-	try {
-		const result = await axios.get('api/post');
-		return result.data.posts;
-	} catch (error) {
-		console.error();
-	}
+export const getVelogPost = async (): Promise<IPost[]> => {
+	const result = await Server.get('api/post');
+	return result.data.posts;
 };
 
-export const getVelogUserInfo = async (): Promise<IUserInfo | undefined> => {
-	try {
-		const result = await axios.get('api/user');
-		return result.data.image;
-	} catch (error) {
-		console.error();
-	}
+export const getVelogUserInfo = async (): Promise<IUserInfo> => {
+	const result = await Server.get('api/user');
+	return result.data.image;
 };
