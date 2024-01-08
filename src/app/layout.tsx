e08d5b7a6669dfaed7b5dfab89342c4cb18e1fe6
rbgksqkr <rbgks1937@naver.com>
2024-01-08 14:27:36 +0900

    feat: noto sans 폰트 설정(#18)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,22 @@
 import './globals.css';
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import { Noto_Sans_KR } from 'next/font/google';
 import NavBar from '@/components/common/NavBar';
-
-const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
 	title: "Gyu's blog",
 	description: 'Gyuhan Park님의 개인 블로그 페이지입니다.',
 };
 
+const notoSansKr = Noto_Sans_KR({
+	weight: ['400', '700'],
+	subsets: ['latin'],
+});
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang='en'>
-			<body className={inter.className}>
+			<body className={notoSansKr.className}>
 				<NavBar />
 				{children}
 			</body>
