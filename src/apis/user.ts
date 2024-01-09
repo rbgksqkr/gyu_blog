049d867056146f817e897b056af531283cca329e
rbgksqkr <rbgks1937@naver.com>
2024-01-09 16:48:12 +0900

    fix: github API 커밋데이터 캐싱데이터 렌더링하는 문제 해결

--- src/apis/user.ts
+++ src/apis/user.ts
@@ -1,9 +1,30 @@
+import { PREV_WEEK, TODAY } from '@/constants';
+import { ICommit } from '@/types/post';
 import { IProject, IUserProfile } from '@/types/user';
+import axios from 'axios';
 import { Server } from './settings';
 
-export const getRecentCommitList = async () => {
-	const result = await Server.get('api/commit');
+const getCommitList = async (): Promise<ICommit[]> => {
+	const today = TODAY.split('T')[0];
+	const prevWeek = PREV_WEEK.split('T')[0];
+	const result = await axios.get(
+		`https://api.github.com/search/commits?q=author:rbgksqkr+committer-date:${prevWeek}..${today}&per_page=100`,
+		{
+			headers: {
+				Authorization: `token ${process.env.NEXT_PUBLIC_GITHUB_TOKEN}`,
+			},
+			params: {
+				sort: 'updated',
+			},
+		},
+	);
 	return result.data.items;
+};
+
+export const getRecentCommitList = async (): Promise<ICommit[]> => {
+	const result = await getCommitList();
+	result.sort((a, b) => new Date(b.commit.author.date).getTime() - new Date(a.commit.author.date).getTime());
+	return result;
 };
 
 export const getProjectList = async (): Promise<IProject[]> => {
