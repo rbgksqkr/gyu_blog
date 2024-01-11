5a20dd96e6ca871bf184e49c23582e7063b68020
rbgksqkr <rbgks1937@naver.com>
2024-01-12 01:39:09 +0900

    feat: 오늘의 커밋 개수와 일주일 간의 커밋 개수 가져오기(#20)

--- src/components/study/WeekCommitContainer.tsx
+++ src/components/study/WeekCommitContainer.tsx
@@ -1,10 +1,29 @@
 import { ICommitContainer } from '@/types/post';
+import Image from 'next/image';
+import Link from 'next/link';
 import styles from './study.module.css';
 
-const WeekCommitContainer = ({ commitWeekList }: { commitWeekList: ICommitContainer[] }) => {
+const WeekCommitContainer = ({
+	todayCommitCount,
+	lastWeekCommitCount,
+	commitWeekList,
+}: {
+	todayCommitCount: number;
+	lastWeekCommitCount: number;
+	commitWeekList: ICommitContainer[];
+}) => {
 	return (
 		<>
-			<div className={styles.commitTitle}> Gyuhan Park의 commit log</div>
+			<div className={styles.commitHeader}>
+				<Link href='https://github.com/rbgksqkr' className={styles.githubLogo}>
+					<Image src='/image/github.png' alt='github logo' width={100} height={100} />
+				</Link>
+				<div className={styles.commitTitle}>Gyuhan Park의 commit log</div>
+				<div className={styles.commitCountWrapper}>
+					<div>TODAY : {todayCommitCount}</div>
+					<div>LAST WEEK : {lastWeekCommitCount}</div>
+				</div>
+			</div>
 			<div className={styles.dayContainer}>
 				{commitWeekList.map((data, idx) => (
 					<div key={idx} className={styles.commitBoxContainer}>
